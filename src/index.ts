a7d9eb735e86bd492154b15f6f45ce43745eb53e
Daniel Antonio Sánchez Jiménez <notrealsolution@gmail.com>
2024-07-06 15:23:43 -0600

    Aplicación del unsubscribe

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,41 @@
-import { Observable, Observer } from "rxjs";
+import { Observable, Observer, interval } from "rxjs";
 const observer: Observer<any> = {
     'next' : value => console.log('next [obs]:', value),
     'error' : error => console.error('error [obs]:', error),
     'complete' : () => console.info('completado [obs]:')
 }
 
+const intervalo$ = new Observable<number>( subscriber => {
+    //Crear un contador 1,2,3,4 ....
+    let count = 0;
+
+    const interval = setInterval(()=>{
+        count++;
+        subscriber.next(count);
+        console.log(count);
+    }, 1000);
+
+    setTimeout( () => {
+        subscriber.complete();
+    }, 2500)
+
+    return () => {
+        clearInterval(interval);
+        console.log('Intervalo destruido');
+    }
+});
 
 
+const subs = intervalo$.subscribe( observer );
+const subs2 = intervalo$.subscribe( observer );
+const subs3= intervalo$.subscribe( observer );
 
+/* subs.add( subs2 )
+        .add(subs3);  Ya no existe add*/
+
+setTimeout(() => {
+    subs.unsubscribe();
+    /* subs2.unsubscribe();
+    subs3.unsubscribe(); */
+    console.log('Completado timeout')
+}, 6000)
---
+++ src/observables/02-unsubscribe.ts
@@ -0,0 +1,8 @@
+import { Observable, Observer, interval } from "rxjs";
+
+const observer: Observer<any> = {
+    'next' : value => console.log('next [obs]:', value),
+    'error' : error => console.error('error [obs]:', error),
+    'complete' : () => console.info('completado [obs]:')
+}
+
